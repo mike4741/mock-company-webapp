4523e64412a95888c535b5db363281e914af20d5
mike4741 <me.menge47@gmail.com>
2023-08-12 12:04:51 -0500

    test2

--- src/main/java/com/mockcompany/webapp/Application.java
+++ src/main/java/com/mockcompany/webapp/Application.java
@@ -5,6 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Application {
+
+    //test
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
